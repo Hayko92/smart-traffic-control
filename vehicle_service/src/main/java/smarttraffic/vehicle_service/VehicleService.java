d8b996ccbaddf8aaa2da29b6e7abc5183cd608f6
Varduhi Sahakyan <sahakyanvarduhy@gmail.com>
2021-10-25 20:00:55 +0400

    remote db connection

---
+++ vehicle_service/src/main/java/smarttraffic/vehicle_service/VehicleService.java
@@ -0,0 +1,13 @@
+package smarttraffic.vehicle_service;
+// draft
+public class VehicleService {
+    int vehicleNumber;
+String model;
+double power;
+int number;
+String texPassport;
+String owner;
+String violation;
+
+
+}
