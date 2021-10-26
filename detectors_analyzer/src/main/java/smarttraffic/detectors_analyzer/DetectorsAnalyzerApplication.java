c8403454611fda86523d52d4e2626fb67d36482d
Hayk <hajko92@mail.ru>
2021-10-26 10:40:14 +0400

    just for pulling

--- detectors_analyzer/src/main/java/smarttraffic/detectors_analyzer/DetectorsAnalyzerApplication.java
+++ detectors_analyzer/src/main/java/smarttraffic/detectors_analyzer/DetectorsAnalyzerApplication.java
@@ -1,6 +1,5 @@
 package smarttraffic.detectors_analyzer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
