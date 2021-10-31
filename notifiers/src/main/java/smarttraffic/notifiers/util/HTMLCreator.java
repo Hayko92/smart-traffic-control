a56ee175978def71f5fc5c429740a386b87e9c3b
Hayk Asatryan <hajko92@mail.ru>
2021-10-31 19:04:51 +0400

    fixed HTNK creaor

--- notifiers/src/main/java/smarttraffic/notifiers/util/HTMLCreator.java
+++ notifiers/src/main/java/smarttraffic/notifiers/util/HTMLCreator.java
@@ -7,62 +7,21 @@ public final class HTMLCreator {
     }
 
     public static String createSpeedViolationBlank(Map<String, String> info) {
-        return String.format("<!DOCTYPE html><html><style>table, th, td { border:1px solid black;} </style><body> <h2>Dear Mr/Ms $s, You have a new speed violation for your vehicle</h2>"
-                        +"<table style=\"width:100%\">" +
-                        "" +
-                        "  <tr>" +
-                        "    <td>id</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "  <tr>" +
-                        "    <td>type</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>platenumber</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>place</td>" +
-                        "    <td>$s</td>" +
-                        "    <tr>" +
-                        "    <td>time</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>vehicle mark</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>vehicle model</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>vehicle year</td>" +
-                        "    <td>$s</td>" +
-                        "    <tr>" +
-                        "    <td>price</td>" +
-                        "    <td><b>$s</b></td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>firstname</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>lastname</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>phone</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "    <tr>" +
-                        "    <td>email</td>" +
-                        "    <td>$s</td>" +
-                        "  </tr>" +
-                        "</table>" +
-                        "</body>" +
-                        "</html>", info.get("surname"),
+        return String.format("Dear Mr/Mrs %s, You have a you violation\n" +
+                        " Violation ID: %s\n" +
+                        "violation type: %s,\n" +
+                        "Vehicle platenumber: %s\n"+
+                        "Violation place: %s\n"+
+                        "Violation time: %s\n"+
+                        "Vehicle mark: %s\n"+
+                        "Vehicle model: %s\n"+
+                        "Vehicle year: %s\n"+
+                        "violation price: %s\n"+
+                        "vehicle owner firstname: %s\n"+
+                        "vehicle owner lastname: %s\n"+
+                        "vehicle phone: %s\n"+
+                        "vehicle email: %s\n",
+                info.get("surname"),
                 info.get("id"),
                 info.get("type"),
                 info.get("plateNumber"),
