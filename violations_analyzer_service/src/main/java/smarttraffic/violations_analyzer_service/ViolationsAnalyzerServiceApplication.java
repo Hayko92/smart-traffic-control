0c674bddfae9fcec7fe44a1b896f5b892a854be9
Hayk Asatryan <hajko92@mail.ru>
2021-11-16 02:33:40 +0400

    added spring profile

--- violations_analyzer_service/src/main/java/smarttraffic/violations_analyzer_service/ViolationsAnalyzerServiceApplication.java
+++ violations_analyzer_service/src/main/java/smarttraffic/violations_analyzer_service/ViolationsAnalyzerServiceApplication.java
@@ -2,10 +2,13 @@ package smarttraffic.violations_analyzer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+
 @EnableMongoRepositories
+@SpringBootApplication
 public class ViolationsAnalyzerServiceApplication {
 
     public static void main(String[] args) {
